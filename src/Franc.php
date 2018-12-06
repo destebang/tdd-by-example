de957cdfd654564a0a379038608e20bdb1addf7f
Daniel Esteban <danielestebangonzalez83@gmail.com>
2018-12-06 17:48:36 +0100

    Add francs

---
+++ src/Franc.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TddByExample;
+
+class Franc
+{
+    /**
+     * @var float
+     */
+    private $amount;
+
+    public function __construct(float $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public function times(int $multiplier): Franc
+    {
+        return new Franc($this->amount * $multiplier);
+    }
+
+    public function amount(): float
+    {
+        return $this->amount;
+    }
+
+    public function equals(Franc $franc): bool
+    {
+        return $this->amount === $franc->amount();
+    }
+}
---
+++ tests/MoneyTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TddByExample\Tests;
+
+use PHPUnit\Framework\TestCase;
+use TddByExample\Dollar;
+use TddByExample\Franc;
+
+class MoneyTest extends TestCase
+{
+    public function testMultiplication(): void
+    {
+        $fiveDollars = new Dollar(5);
+        $this->assertEquals(new Dollar(10), $fiveDollars->times(2));
+        $this->assertEquals(new Dollar(15), $fiveDollars->times(3));
+    }
+
+    public function testEquality(): void
+    {
+        $fiveDollars = new Dollar(5);
+        $this->assertTrue($fiveDollars->equals(new Dollar(5)));
+        $this->assertFalse($fiveDollars->equals(new Dollar(6)));
+    }
+
+    public function testFrancMultiplication(): void
+    {
+        $fiveFrancs = new Franc(5);
+        $this->assertEquals(new Franc(10), $fiveFrancs->times(2));
+        $this->assertEquals(new Franc(15), $fiveFrancs->times(3));
+    }
+}
