70bf6e4ac3859eb591bc9b925505396365d36e39
Daniel Esteban <danielestebangonzalez83@gmail.com>
2018-12-06 18:53:10 +0100

    Remove franc and dollar subclass. Remove redundant tests

--- src/Franc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace TddByExample;
-
-class Franc extends Money
-{
-}
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -21,28 +21,12 @@ class MoneyTest extends TestCase
     {
         $this->assertTrue(Money::dollar(5)->equals(Money::dollar(5)));
         $this->assertFalse(Money::dollar(5)->equals(Money::dollar(6)));
-
-        $this->assertTrue((Money::franc(5))->equals(Money::franc(5)));
-        $this->assertFalse(Money::dollar(5)->equals(Money::franc(6)));
-
         $this->assertFalse(Money::dollar(5)->equals(Money::franc(5)));
-    }
-
-    public function testFrancMultiplication(): void
-    {
-        $fiveFrancs = Money::franc(5);
-        $this->assertEquals(Money::franc(10), $fiveFrancs->times(2));
-        $this->assertEquals(Money::franc(15), $fiveFrancs->times(3));
     }
 
     public function testCurrency(): void
     {
         $this->assertEquals("USD", Money::dollar(1)->currency());
         $this->assertEquals("CHF", Money::franc(1)->currency());
-    }
-
-    public function testDifferentClassEquality(): void
-    {
-        $this->assertTrue((new Money(10, "CHF"))->equals(new Franc(10, "CHF")));
     }
 }
